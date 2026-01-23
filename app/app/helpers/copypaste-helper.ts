6112e8fb4b573b83f326d66921279869b96ea53e
Filippo <133808513+FilippoDude@users.noreply.github.com>
2026-01-23 19:57:54 +0100

    Made working text change with indexes

--- app/app/helpers/copypaste-helper.ts
+++ app/app/helpers/copypaste-helper.ts
@@ -1,11 +1,23 @@
-interface TextUpdateType {
+interface TextUpdate {
   index: number;
-  added?: string;
-  deleted?: string;
+  added: string;
+  deleted: string;
 }
 
 export const CopypasteHelper = {
   reconstructedText: <string>"",
+
+  TESTaddOrRemoveToOld(textUpdate: TextUpdate) {
+    const firstPart = this.reconstructedText.substring(0, textUpdate.index);
+    const secondPart = this.reconstructedText.substring(
+      textUpdate.index + textUpdate.deleted.length,
+      this.reconstructedText.length,
+    );
+
+    const textWithRemoved = firstPart + textUpdate.added + secondPart;
+    console.log(textWithRemoved);
+    this.reconstructedText = textWithRemoved;
+  },
 
   getDifferenceStart(newText: string, oldText: string): number {
     const lengthToCheck = Math.min(newText.length, oldText.length);
@@ -17,27 +29,34 @@ export const CopypasteHelper = {
     return i;
   },
 
-  getTextDifferences(newText: string, oldText: string): TextUpdateType | null {
+  // Returns added and deleted text between two texts
+  getTextDifferences(newText: string, oldText: string): TextUpdate | null {
     const start = this.getDifferenceStart(newText, oldText);
-    console.log("IT STARTS AT: " + start?.toString());
 
     let oldEnd = oldText.length - 1;
     let newEnd = newText.length - 1;
-    while (newEnd >= 0 && oldEnd >= 0 && oldText[oldEnd] === newText[newEnd]) {
+    while (
+      newEnd >= start &&
+      oldEnd >= start &&
+      oldText[oldEnd] === newText[newEnd]
+    ) {
       oldEnd--;
       newEnd--;
     }
 
     const deleted = oldText.slice(start, oldEnd + 1);
-    const inserted = newText.slice(start, newEnd + 1);
+    const added = newText.slice(start, newEnd + 1);
 
+    console.log("IT STARTS AT: " + start.toString());
     console.log("DELETED: " + deleted);
-    console.log("INSERTED: " + inserted);
+    console.log("INSERTED: " + added);
 
-    let finalObj: TextUpdateType = {
+    let finalObj: TextUpdate = {
       index: start,
+      added: added,
+      deleted: deleted,
     };
-
+    this.TESTaddOrRemoveToOld(finalObj);
     return finalObj;
   },
 };
